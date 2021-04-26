ffce21815003a01faf1866eb1ac636fa1e556df1
Gobi <gobi615@gmail.com>
2021-04-27 01:14:25 +0530

    cors

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
 const express = require('express') ;
 const api = require('./api')
-
+const middleware = require('./middleware');
 const app = express();
 
-app.get('/all', api.getAll);
+
 
 app.listen(process.env.PORT || 8080, ()=>console.log('server started!!'))
-
+app.use(middleware.cors)
+app.get('/all', api.getAll);
 console.log('env:'+process.env)
